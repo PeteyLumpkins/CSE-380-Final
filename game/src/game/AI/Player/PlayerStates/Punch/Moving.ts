5a0f96b19f57c8e558c85ed99001f8e960eea8cc
Han <79167052+hannert@users.noreply.github.com>
2022-04-19 20:07:10 -0400

    Updated player sprite
    Added player sprite for attacking,
    organized JSON.

---
+++ game/src/game/AI/Player/PlayerStates/Punch/Moving.ts
@@ -0,0 +1,32 @@
+import State from "../../../../../Wolfie2D/DataTypes/State/State";
+import StateMachineAI from "../../../../../Wolfie2D/AI/StateMachineAI";
+import StateMachine from "../../../../../Wolfie2D/DataTypes/State/StateMachine";
+import GameNode from "../../../../../Wolfie2D/Nodes/GameNode";
+import GameEvent from "../../../../../Wolfie2D/Events/GameEvent";
+import Input from "../../../../../Wolfie2D/Input/Input";
+import Timer from "../../../../../Wolfie2D/Timing/Timer";
+import Vec2 from "../../../../../Wolfie2D/DataTypes/Vec2";
+import { PlayerStates } from "../../PlayerController";
+import PlayerState from "../PlayerState";
+
+
+
+export default abstract class Attacking extends PlayerState {
+
+    update(deltaT: number): void {
+        super.update(deltaT);
+
+        let dir = this.getInputDirection();
+        if(dir.isZero()){
+            this.idle()
+        } 
+        
+        this.owner.move(dir.mult(new Vec2(this.speedScale, this.speedScale)));
+        this.move(dir);
+    }
+
+    abstract idle(): void;
+
+    abstract move(dir: Vec2): void;
+
+}
