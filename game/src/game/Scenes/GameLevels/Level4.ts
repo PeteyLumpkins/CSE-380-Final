844e19c677b0f64aa0a96016a8a20eef1eeef819
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-05-09 14:06:34 -0400

    Revert "Finalbenchmark"

--- game/src/game/Scenes/GameLevels/Level4.ts
+++ game/src/game/Scenes/GameLevels/Level4.ts
@@ -45,7 +45,8 @@ export default class Level4 extends GameLevel {
         this.load.tilemap("level", "assets/tilemaps/Level32.json");
         this.load.object(GameData.NAVMESH, "assets/data/navmeshLevel32.json");
         this.load.object("enemyData", "assets/data/enemyLevel32.json");
-        this.load.audio("level3", "assets/music/level3.wav");
+        this.load.audio("level3", "assets/music/Level3.wav");
+
     }
 
     unloadScene(): void {
